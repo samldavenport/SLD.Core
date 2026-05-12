06b02daf918f90124d3ce61f05e84d8f8dfe6b1d
thereclaimer <sam.davenport@thereclaimergame.com>
2026-05-12 07:23:48 -0500

    initial checkin

---
+++ include/sld.hpp
@@ -0,0 +1,212 @@
+#ifndef SLD_HPP
+#define SLD_HPP
+
+#include <Windows.h>
+#include <cstdint>
+#include <assert.h>
+
+#ifdef SLD_STATIC
+#   define SLD_API
+#elif defined(SLD_DLL_EXPORT)
+#   define SLD_API __declspec(dllexport)
+#else
+#   define SLD_API __declspec(dllimport)
+#endif
+
+#define SLD_API_INLINE  static inline
+#define SLD_API_UTILITY static constexpr
+
+#if _MSC_VER
+#   if !defined(__clang__)
+#       define INSTRUCTION_REORDER_BARRIER _ReadWriteBarrier()
+#   endif
+#   include <intrin.h>
+#else
+#   include <x86intrin.h>
+#endif
+
+namespace sld {
+
+    //--------------------------------------------------------------------
+    // PRIMITIVE TYPES
+    //--------------------------------------------------------------------
+
+    // signed integers
+    typedef int8_t    s8;
+    typedef int16_t   s16;
+    typedef int32_t   s32;
+    typedef int64_t   s64;
+
+    // unsigned integers
+    typedef uint8_t   u8;
+    typedef uint16_t  u16;
+    typedef uint32_t  u32;
+    typedef uint64_t  u64;
+
+    // floats
+    typedef float    f32;
+    typedef double   f64;
+
+    // simd
+    typedef __m128i  simd_4xu32;
+    typedef __m128d  simd_2xf64;
+    typedef __m128   simd_4xf32;
+
+    // booleans
+    typedef u8       b8;
+    typedef u16      b16;
+    typedef u32      b32;
+    typedef u64      b64;
+
+    // characters
+    typedef char     cchar8;
+    typedef wchar_t  wchar16;
+
+    // memory
+    typedef u8       byte;
+    typedef vptr     void*;
+    typedef intptr_t addr;
+    typedef u32      index;
+    typedef void     element;
+    typedef void     key;
+    typedef void     value;
+
+    // handles
+    typedef u8       h8;
+    typedef u16      h16;
+    typedef u32      h32;
+    typedef u64      h64;
+
+    //--------------------------------------------------------------------
+    // MEMORY
+    //--------------------------------------------------------------------
+
+    struct memory {
+        u64 size;
+        union {
+            void* ptr;
+            byte* bytes;
+            addr  address;
+        };
+    };
+
+    //--------------------------------------------------------------------
+    // LARGE VALUES
+    //--------------------------------------------------------------------
+
+    struct alignas(16) u128 {
+        union {
+            struct {
+                u32 u32_0;
+                u32 u32_1;
+                u32 u32_2;
+                u32 u32_3;
+            };
+            struct {
+                u64 u64_lo;
+                u64 u64_hi;
+            };
+            u64        val64[2];
+            u32        val32[4];
+            simd_4xu32 simd_reg;
+        };
+    };
+
+    struct alignas(16) f128 {
+        union {
+            struct {
+                f32 f32_0;
+                f32 f32_1;
+                f32 f32_2;
+                f32 f32_3;
+            };
+            struct {
+                f64 u64_lo;
+                f64 u64_hi;
+            };
+            f64         val64[2];
+            f32         val32[4];
+            simd_4xuf32 simd_reg;
+        };
+    };
+
+    //--------------------------------------------------------------------
+    // GRAPHICS
+    //--------------------------------------------------------------------
+
+    struct color_rgba_u32 {
+        union {
+            struct {
+                u32 r;
+                u32 g;
+                u32 b;
+                u32 a;
+            };
+            u32 val;
+        };
+    };
+
+    struct color_rgba_f32 {
+        f32 r;
+        f32 g;
+        f32 b;
+        f32 a;
+    };
+
+    struct size {
+        u32 width;
+        u32 height;
+    };
+
+    typedef struct {
+        u32 x;
+        u32 y; 
+    } point, position;
+
+    struct dimensions {
+        size     size;
+        position pos;
+    };
+
+    //--------------------------------------------------------------------
+    // BITWISE LOGIC
+    //--------------------------------------------------------------------
+
+
+    SLD_API_UTILITY u32  bit_value     (const u32 bit)                                     { return (1 << bit);                                            }
+    SLD_API_UTILITY bool bit_test      (const u32 value, const u32 bit)                    { return ((value >> bit) & 1);                                  }
+    SLD_API_UTILITY void bit_on        (u32&      value, const u32 bit)                    { value |=  (1 << bit);                                         }
+    SLD_API_UTILITY void bit_off       (u32&      value, const u32 bit)                    { value &= ~(1 << bit);                                         }
+    SLD_API_UTILITY void bit_toggle    (u32&      value, const u32 bit)                    { value ^=  (1 << bit);                                         }
+    SLD_API_UTILITY void bit_set       (u32&      value, const u32 bit , const bool state) { value = (state) ? (value | (1 << bit)) : value & ~(1 << bit); }
+    SLD_API_UTILITY bool bit_mask_test (const u32 value, const u32 mask)                   { return((value & mask) > 0);                                   }
+    SLD_API_UTILITY void bit_mask_and  (u32&      value, const u32 mask)                   { (value |=  mask);                                             }
+    SLD_API_UTILITY void bit_mask_or   (u32&      value, const u32 mask)                   { (value &= ~mask);                                             }
+
+    //--------------------------------------------------------------------
+    // SIMD
+    //--------------------------------------------------------------------
+
+    SLD_API_INLINE f128 simd_u128_a_add_b  (u128& reg_a, const u128& reg_b) { return(_mm_add_epi32(reg_a, reg_b)); }
+    SLD_API_INLINE f128 simd_u128_a_sub_b  (u128& reg_a, const u128& reg_b) { return(_mm_sub_epi32(reg_a, reg_b)); }
+    SLD_API_INLINE f128 simd_u128_a_mul_b  (u128& reg_a, const u128& reg_b) { return(_mm_mul_epi32(reg_a, reg_b)); }
+    SLD_API_INLINE f128 simd_u128_a_div_b  (u128& reg_a, const u128& reg_b) { return(_mm_div_epi32(reg_a, reg_b)); }
+    
+    SLD_API_INLINE f128 simd_f128_a_add_b  (f128& reg_a, const u128& reg_b) { return(_mm_add_ps(reg_a, reg_b));    }
+    SLD_API_INLINE f128 simd_f128_a_sub_b  (f128& reg_a, const u128& reg_b) { return(_mm_sub_ps(reg_a, reg_b));    }
+    SLD_API_INLINE f128 simd_f128_a_mul_b  (f128& reg_a, const u128& reg_b) { return(_mm_mul_ps(reg_a, reg_b));    }
+    SLD_API_INLINE f128 simd_f128_a_div_b  (f128& reg_a, const u128& reg_b) { return(_mm_div_ps(reg_a, reg_b));    }
+    SLD_API_INLINE f128 simd_f128_sqrt     (const f128& reg)                { return(_mm_sqrt_ps(reg));            }
+
+    //--------------------------------------------------------------------
+    // HASHING
+    //--------------------------------------------------------------------
+
+    SLD_API u32  hash_u32      (const void* data,   const u32   size);
+    SLD_API u64  hash_u64      (const void* data,   const u32   size);
+    SLD_API u128 hash_u128     (const void* data,   const u32   size);
+    SLD_API bool hash_is_equal (const void* data_a, const void* data_b, const u32 size);
+
+};
+
+#endif //SLD_HPP
---
+++ src/hash.cpp
@@ -0,0 +1,71 @@
+#include "sld.hpp"
+
+#include <meow-hash/meow_hash_x64_aesni.h>
+
+namespace sld {
+
+    SLD_API u32
+    hash_u32(
+        const void* data,
+        const u32   size) {
+
+        assert(data != NULL && size != 0);
+
+        u128 hash;
+        hash.simd_reg = MeowHash(MeowDefaultSeed, size, data);
+
+        return(hash.u32_0);
+    }
+
+    SLD_API u64
+    hash_u64(
+        const void* data,
+        const u32   size) {
+
+        assert(data != NULL && size != 0);
+
+        u128 hash;
+        hash.simd_reg = MeowHash(MeowDefaultSeed, size, data);
+
+        return(hash.u64_0);
+    }
+
+    SLD_API u128
+    hash_u128(
+        const void* data,
+        const u32   size) {
+
+        assert(data != NULL && size != 0);
+
+        u128 hash;
+        hash.simd_reg = MeowHash(MeowDefaultSeed, size, data);
+
+        return(hash);
+    }
+
+    SLD_API bool
+    hash_is_equal(
+        const void* data_a,
+        const void* data_b,
+        const u32   size) {
+
+        assert(
+            data_a != NULL &&
+            data_b != NULL &&
+            size   != 0
+        );
+
+        u128 hash_a, hash_b;
+
+        hash_a.simd_reg = MeowHash(MeowDefaultSeed, size, data_a);
+        hash_b.simd_reg = MeowHash(MeowDefaultSeed, size, data_b);
+
+        const bool is_equal = (
+            hash_a.u64_lo == hash_b.u64_lo &&
+            hash_a.u64_hi == hash_b.u64_hi &&
+        );
+
+        return(is_equal);
+    }
+
+};
